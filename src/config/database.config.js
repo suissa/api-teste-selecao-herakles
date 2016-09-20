390f9427b602f4557297238fa1977666e995b134
Lucas França <lucashytec@gmail.com>
2016-09-20 15:09:09 -0300

    Alterando db

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -1,10 +1,7 @@
 var Sequelize = require('sequelize');
 /**CONNECTION DATABASE**/
-/*
+
 module.exports = new Sequelize('herakles_selecao', 'postgres', 'postgres', {
 	host: 'localhost',
 	dialect: 'postgres'
 });
-*/
-
-module.exports = new Sequelize('postgres://tvwhcsjumaeexg:ztOh94bL_qJkhhLPvbTujW1ypf@ec2-54-243-47-46.compute-1.amazonaws.com:5432/dft0u61rsgdj5f');

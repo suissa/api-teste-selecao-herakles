bf259b5d8c0cede67a2fdf19a48a9df03aad1b3e
Suissa <jnascimento@gmail.com>
2016-10-30 09:55:18 -0200

    Refatoração das rotas do auth

--- src/modules/auth/route.js
+++ src/modules/auth/route.js
@@ -3,12 +3,8 @@ var router = express.Router();
 var Ctrl = require('./controller');
 
 /**LOGIN**/
-router.post('/login', function(req, res) {
-	Ctrl.login(req,res);
-});
+router.post('/login', Ctrl.login);
 /**NEW USER**/
-router.post('/signup', function(req, res) {
-	Ctrl.signup(req,res);
-});
+router.post('/signup', Ctrl.signup);
 
 module.exports = router;
